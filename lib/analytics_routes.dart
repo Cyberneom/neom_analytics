845e89c98cacececa1df4b32a569fe3707100cc4
Emmanuel Montoya <yori.technologies@gmail.com>
2025-07-08 01:22:38 -0600

    First commit for analytics module

--- lib/analytics_routes.dart
+++ lib/analytics_routes.dart
@@ -1,11 +1,7 @@
 import 'package:get/get.dart';
 import 'package:neom_core/utils/constants/app_route_constants.dart';
-import '../analytics/ui/analytics_page.dart';
-import '../media/media_fullscreen_page.dart';
+
 import 'ui/analytics_page.dart';
-import 'ui/previous_version_page.dart';
-import 'ui/splash_page.dart';
-import 'ui/under_construction_page.dart';
 
 class CommonRoutes {
 
